90ea55770bff39fc43cf60655c3a743c766cc98e
nkneeraj <nkneeraj641@gmail.com>
2020-09-24 13:03:27 +0530

    [Neeraj Kumar]Created - UC1-AttendanceCheck

--- src/com/CG/WageProblem/EmpBuilder.java
+++ src/com/CG/WageProblem/EmpBuilder.java
@@ -3,7 +3,13 @@ package com.CG.WageProblem;
 public class EmpBuilder {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		int IS_FULL_TIME =1;
+		double empCheck = Math.floor(Math.random()*10)%2;
+		if(empCheck == IS_FULL_TIME)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
+
 	}
 
 }
